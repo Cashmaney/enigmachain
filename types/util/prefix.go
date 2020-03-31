33657b4f9975275f00a88e2aebe771de1e1eeb11
Dan <dbriggsie@gmail.com>
2020-03-30 23:41:05 -0400

    Update prefix.go

--- types/util/prefix.go
+++ types/util/prefix.go
@@ -2,15 +2,15 @@ package util
 
 const (
 	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
-	Bech32PrefixAccAddr = "enigma"
+	Bech32PrefixAccAddr = "kamut"
 	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
-	Bech32PrefixAccPub = "enigmapub"
+	Bech32PrefixAccPub = "kamutpub"
 	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
-	Bech32PrefixValAddr = "enigmavaloper"
+	Bech32PrefixValAddr = "kamutvaloper"
 	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
-	Bech32PrefixValPub = "enigmavaloperpub"
+	Bech32PrefixValPub = "kamutvaloperpub"
 	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
-	Bech32PrefixConsAddr = "enigmavalcons"
+	Bech32PrefixConsAddr = "kamutvalcons"
 	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
-	Bech32PrefixConsPub = "enigmavalconspub"
+	Bech32PrefixConsPub = "kamutvalconspub"
 )
